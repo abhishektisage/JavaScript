24a6f1ee32357e0b21dd396197e37b3e270acf6d
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-02-21 19:07:35 +0530

    Add files via upload

---
+++ arith.js
@@ -0,0 +1,15 @@
+let a=17;
+let b=10;
+console.log(a);
+console.log(b);
+console.log("Arithmetic Operations");
+let sum=a+b;
+console.log(sum);
+let diff=a-b;
+console.log(diff);
+let mul=a*b;
+console.log(mul);
+let div=a/b;
+console.log(div);
+let pow=a**b;
+console.log(pow);
