627b2cc25f0a26d60f40b415f85534c2c9de5f82
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-02-21 22:42:23 +0530

    Add files via upload

---
+++ traffic.js
@@ -0,0 +1,10 @@
+let light="red";
+if(light=="red"){
+   console.log("Stop");
+}
+if(light=="yellow"){
+    console.log("Slow your vehicle");
+}
+if(light=="green"){
+    console.log("you can go")
+}
