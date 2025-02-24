bbf829303c89eecdafea6ce9cb8048434fa11a0d
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-02-24 22:53:49 +0530

    Add files via upload

---
+++ size.js
@@ -0,0 +1,13 @@
+let size="XL"
+if(size=="XL"){
+    console.log("Price is 250rs.");
+}
+if(size=="L"){
+    console.log("Price is 200rs.");
+}
+if(size=="M"){
+    console.log("Price is 100rs.");
+}
+if(size=="S"){
+    console.log("Price is 50rs.");
+}
