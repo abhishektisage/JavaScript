a919dc9fb12938d2bba35eca866b7fe3b5ff5fcb
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-02-26 21:20:17 +0530

    Add files via upload

---
+++ lastdigit.js
@@ -0,0 +1,12 @@
+let num1=1708;
+let num2=2908;
+let ls1=num1/10;
+console.log(ls1);
+let ls2=num2%10;
+console.log(ls2);
+if(ls1==ls2){
+    console.log("Last digit of both numbers are same");
+}
+else{
+    console.log("Last digit of both numbers are same");
+}
