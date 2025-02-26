f5234adcdfd952a58cef2b569c79b8e6a11226ed
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-02-26 21:21:04 +0530

    Add files via upload

---
+++ string1.js
@@ -0,0 +1,8 @@
+let str=prompt("Enter any string");
+console.log(str);
+if((str[0]=='A' || str[0]=='a') && str.length>5){
+    console.log("it is a  golden string");
+}
+else{
+    console.log("it is not a golden string");
+}
