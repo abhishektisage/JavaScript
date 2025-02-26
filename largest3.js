0034850c80dd6eebfe24e99ccad8eae8553334d0
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-02-26 21:20:39 +0530

    Add files via upload

---
+++ largest3.js
@@ -0,0 +1,22 @@
+let a=17
+let b=69
+let c=9
+console.log(a);
+console.log(b);
+console.log(c);
+if(a>b){
+    if(a>c){
+    console.log(`a= ${a} is the largest number`);
+    }
+    else{
+        console.log(`c= ${c} is the largest number`);
+    }
+}
+else{
+    if(b>c){
+        console.log(`b=${b} is largest number`);
+    }
+    else{
+        console.log(`c =${c} is the largest number`);
+    }
+}
