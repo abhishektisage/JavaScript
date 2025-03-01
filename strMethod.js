471deccc2566d83f20ee8f25596962177acbb770
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-03-01 20:18:17 +0530

    Add files via upload

---
+++ strMethod.js
@@ -0,0 +1,11 @@
+let name="jnec     ";
+console.log(name.trim().toUpperCase());
+
+let str="Abhishek";
+let a1=str.slice(0,4);
+let a2=str.indexOf("Abhi");
+let a3=str.replace("shek","-Snow");
+console.log(a1,a2,a3);
+
+let str1="apnacollege";
+console.log(str1.slice(4).replace('c','k'));
