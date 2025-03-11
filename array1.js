7090de12b26f32d9cade948e0e3608638acc3194
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-03-11 23:56:06 +0530

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,9 @@
+let month=["january","july","march","august"];
+month.splice(0,2,"july","june");
+console.log(month);
+
+let lang=["c","c++","javascript","python","java","c#","sql"];
+lang.reverse();
+let i=lang.indexOf("javascript");
+console.log(i);
+
