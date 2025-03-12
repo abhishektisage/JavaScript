5766975765adad8b4ef150d67dc78a6ccc01cfe8
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-03-12 23:41:00 +0530

    Add files via upload

---
+++ testchar.js
@@ -0,0 +1,12 @@
+let str=prompt("enter any string");
+
+let index = 5;
+
+ if(str[index] === str[index].toLowerCase() && str[index] !== str[index].toUpperCase()){
+       console.log("char is lowercase");
+ }
+ else{
+    console.log("char is not lowercase");
+ }
+
+
