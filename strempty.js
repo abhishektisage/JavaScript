835a0685a09efb8a744f015008c16b032ed35aca
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-03-12 23:40:08 +0530

    Add files via upload

---
+++ strempty.js
@@ -0,0 +1,9 @@
+let str=prompt("Enter any name");
+let len=str.length;
+console.log(len);
+if(str.length==0){
+    console.log("string is empty");
+}
+else{
+    console.log("string is not emmpty");
+}
