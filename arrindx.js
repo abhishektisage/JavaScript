79a76eee5f3d69d821f2e8d1ee57efad4a84d5ed
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-03-13 19:07:31 +0530

    Add files via upload

---
+++ arrindx.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+const searchElement = 3;
+
+if (numbers.includes(searchElement)) {
+    console.log(`${searchElement} exists in the array.`);
+} else {
+    console.log(`${searchElement} does not exist in the array.`);
+}
