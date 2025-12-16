cd08107da1e20634a79371a37c2be28fb41293e3
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-12-16 21:17:25 +0530

    Add files via upload

---
+++ FUNCTIONS/arr.js
@@ -0,0 +1,9 @@
+const arrayAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+let arr=[5,10,15];
+console.log(arrayAverage(arr));
